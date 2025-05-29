d5e23847adbb03e8caf82678c1dc067d1680bcf9
ashutosh <ashutoshpandey7556@gmail.com>
2025-05-29 19:01:23 +0530

    3rd commit

---
+++ app/Events/RealTimeMessage.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class RealTimeMessage implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public string $message;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     */
+    public function broadcastOn(): Channel
+    {
+        return new Channel('events'); // Public channel "events"
+    }
+
+    /**
+     * Optional: Customize the broadcast name
+     */
+    public function broadcastAs(): string
+    {
+        return 'real-time.message';
+    }
+}
